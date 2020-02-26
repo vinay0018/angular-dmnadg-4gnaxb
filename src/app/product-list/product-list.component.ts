cf4c61780b8e3b4eaf8683f2c024448ee6ad5f09
vinay0018 <vinay0018@gmail.com>
2020-02-25 23:57:24 -0600

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -55,16 +55,15 @@ export class ProductListComponent implements OnInit {
   printComponent(cmpName) {
      let printContents = document.getElementById(cmpName).innerHTML;
      let originalContents = document.body.innerHTML;
-
      document.body.innerHTML = printContents;
-
      window.print();
-
      document.body.innerHTML = originalContents;
 }
 
 
-
+//  deleteItems() {
+//   localStorage.clear(this.totalPrice);
+//  }
 }
 
 
